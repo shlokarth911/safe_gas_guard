7b39e497d5c697e9132c820c6d08becfff0c7133
Shlok Arth <166167326+shlokarth911@users.noreply.github.com>
2024-10-25 13:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,500 +1,216 @@
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  color: white;
-  font-family: PP-Mori;
-  text-decoration: none;
-  list-style: none;
-}
-
-@font-face {
-  font-family: PP-Mori;
-  src: url(./PPMori-Regular.woff);
-}
-
-html,
-body {
-  height: 100%;
-  width: 100vw;
-  overflow-x: hidden;
-  background-color: #080808;
-}
-
-::-webkit-scrollbar {
-  width: 0;
-}
-
-main {
-  background-image: url(./MacBook\ Air\ -\ 1.png);
-  background-size: 100%;
-  background-repeat: no-repeat;
-  position: relative;
-}
-
-nav {
-  padding: 0.8vw 2vw;
-  background-color: rgba(0, 0, 0, 0.418);
-  backdrop-filter: blur(20px);
-  position: fixed;
-  top: 5%;
-  left: 50%;
-  transform: translateX(-50%);
-  border-radius: 2vw;
-  width: 30vw;
-  height: 3vw;
-  overflow: hidden;
-  z-index: 999;
-  border: 1px solid #4b4b4b;
-}
-
-nav h4 {
-  font-size: 0.9vw;
-}
-
-nav .top {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 2vw;
-}
-
-nav .top h1 {
-  font-size: 1.5vw;
-  font-weight: 400;
-}
-
-nav .top button {
-  font-size: 1.5vw;
-  font-weight: 400;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-}
-
-.nav-main {
-  /* overflow: hidden; */
-  width: 80vw;
-  padding: 3vw 3.5vw;
-  display: flex;
-  justify-content: space-between;
-  text-wrap: nowrap;
-}
-
-.nav-main-part1 {
-  width: 28%;
-}
-
-.nav-main-part1 h4 {
-  color: rgb(170, 170, 170);
-  font-weight: 400;
-  padding: 1vw;
-  border-bottom: 1px solid rgb(170, 170, 170);
-}
-
-.nav-main-part1 ul {
-  padding: 1vw;
-  overflow: hidden;
-}
-
-.nav-main-part1 ul li {
-  list-style: none;
-  margin-top: 1vw;
-  margin-bottom: 1vw;
-  font-size: 1.8vw;
-  font-weight: 300;
-}
-
-.nav-main-part1 ul li::after {
-  content: "";
-  display: block;
-  position: relative;
-  height: 0.05vw;
-  width: 0%;
-  background-color: white;
-  transition: all 0.3s;
-}
-
-.nav-main-part1 ul li:hover::after {
-  content: "";
-  display: block;
-  position: relative;
-  height: 0.05vw;
-  width: 100%;
-  background-color: white;
-}
-
-.nav-footer {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 2vw 0;
-  padding-bottom: 0.8vw;
-}
-
-.nav-footer h4 {
-  font-weight: 300;
-}
-
-.nav-footer ul {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1vw;
-}
-
-.nav-footer ul li {
-  position: relative;
-  font-size: 0.9vw;
-}
-
-.nav-footer ul li::after {
-  content: "";
-  display: block;
-  position: relative;
-  height: 0.05vw;
-  width: 0%;
-  background-color: white;
-  transition: all 0.2s;
-}
-
-.nav-footer ul li:hover:after {
-  content: "";
-  display: block;
-  position: relative;
-  height: 0.05vw;
-  width: 100%;
-  background-color: white;
-}
-
-.hero {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
-
-.hero h1 {
-  margin-top: 13vw;
-  text-wrap: nowrap;
-  text-align: center;
-  font-size: 10vw;
-  background-image: linear-gradient(to right, #366afc, #a156dc);
-  background-clip: text;
-  color: transparent;
-  line-height: 92%;
-  padding: 1vw 0;
-}
-
-.hero h2 {
-  font-size: 2vw;
-  font-weight: 400;
-  text-align: center;
-  margin-top: 2vw;
-}
-
-.about {
-  margin-top: 12vw;
-  padding: 0 3.5vw;
-}
-
-.about h2 {
-  font-size: 3.5vw;
-  font-weight: 500;
-}
-
-.about p {
-  width: 90%;
-  font-size: 2vw;
-  line-height: 120%;
-  margin-top: 2vw;
-}
-
-.about p span {
-  display: inline-block;
-  width: 10vw;
-}
-
-.string {
-  height: 20vw;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-}
-
-.string svg {
-  mix-blend-mode: difference;
-  position: relative;
-  overflow: visible;
-  width: fit-content;
-  z-index: 9;
-}
-
-.features {
-  padding: 0 3.5vw;
-  margin-top: 2vw;
-}
-
-.features h1 {
-  font-size: 5vw;
-  font-weight: 300;
-}
-
-.container {
-  padding: 0vw 5vw;
-}
-
-.drop-line {
-  width: 0.3%;
-  height: 10vw;
-  background-color: rgb(255, 255, 255);
-  background-image: linear-gradient(
-    to top,
-    #080808 1%,
-    #ffffff 90%,
-    #080808 1%
-  );
-  margin-top: 5vw;
-  margin-bottom: 5vw;
-  position: relative;
-}
-
-.card-header {
-  display: flex;
-  align-items: center;
-  gap: 2vw;
-}
-
-.text-drop-line {
-  display: flex;
-  margin: 5vw 0;
-  gap: 5.5vw;
-  position: relative;
-}
-
-.text-drop-line p {
-  font-size: 3.4vw;
-  font-weight: 300;
-  font-weight: 700;
-  line-height: 120%;
-}
-
-.orange {
-  color: #ff6012;
-  text-shadow: 0 0 40px #ff6012;
-}
-
-.blue {
-  color: #128cff;
-  text-shadow: 0 0 40px #128cff;
-}
-
-.green {
-  color: #03df41;
-  text-shadow: 0 0 40px #03df41;
-}
-
-.megenta {
-  color: #d51c80;
-  text-shadow: 0 0 40px #d51c80;
-}
-
-.white {
-  color: rgb(255, 255, 255);
-  text-shadow: 0 0 40px rgb(255, 255, 255);
-}
-
-.text-drop-line .drop-line {
-  margin: 0;
-  height: 20vw;
-}
-
-.card-header h3 {
-  font-size: 1.7vw;
-  font-weight: 300;
-}
-
-.card-header img {
-  width: 4vw;
-  transform: translateX(-50%);
-}
-
-.orange-glow {
-  filter: drop-shadow(0 0 20px #ff6012);
-}
-
-.blue-glow {
-  filter: drop-shadow(0 0 30px #0084ff);
-}
-
-.green-glow {
-  filter: drop-shadow(0 0 30px #00ff48);
-}
-
-.megenta-glow {
-  filter: drop-shadow(0 0 20px #d51c80);
-}
-
-.white-glow {
-  filter: drop-shadow(0 0 20px rgb(255, 255, 255));
-}
-
-.slogan {
-  margin-top: 5vw;
-  padding: 5vw 15vw;
-}
-
-.slogan h1 {
-  font-size: 10vw;
-  text-align: center;
-  font-weight: 500;
-  text-shadow: 0 0 70px rgb(175, 175, 175);
-}
-
-.demo {
-  margin-top: 5vw;
-  padding: 0 3.5vw;
-  position: relative;
-}
-
-.demo h1 {
-  font-size: 3vw;
-}
-
-.demo-vid {
-  width: 100%;
-  height: 40vw;
-  background-color: rgb(53, 53, 53);
-  border-radius: 1vw;
-  margin-top: 2vw;
-}
-
-.demo button {
-  background-color: #fff;
-  color: rgb(0, 0, 0);
-  font-size: 1.5vw;
-  font-weight: 600;
-  padding: 1vw 0.5vw;
-  padding-left: 1vw;
-  border-radius: 10vw;
-  border: none;
-  margin-top: 7vw;
-  transition: all 0.2s;
-  position: relative;
-  left: 50%;
-  transform: translate(-50%);
-}
-
-.demo i {
-  color: rgb(255, 255, 255);
-  background-color: black;
-  border: 1px solid black;
-  padding: 0.5vw;
-  border-radius: 50%;
-  margin-left: 1vw;
-}
-
-.demo button:hover {
-  background-color: rgb(0, 0, 0);
-  color: #fff;
-}
-
-footer {
-  margin-top: 12vw;
-  padding: 0 3.5vw;
-  padding-bottom: 5vw;
-}
-
-footer h1 {
-  font-size: 6vw;
-  font-weight: 500;
-}
-
-.footer-parts {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 8vw;
-}
-
-.footer-part1 {
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: column;
-  gap: 2vw;
-}
-
-.footer-part1 a {
-  background-color: white;
-  color: black;
-  font-size: 2vw;
-  height: 5vw;
-  width: 30vw;
-  border-radius: 10vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 800;
-  transition: all 0.25s;
-  border: 1px solid transparent;
-}
-
-.footer-part1 a:hover {
-  background-color: black;
-  color: white;
-  border-radius: 1vw;
-  border: 1px solid white;
-  box-shadow: -10px 10px white;
-  transform: translate(10px, -10px);
-}
-
-.footer-part2 {
-  width: 20%;
-}
-
-.footer-part2 h4 {
-  font-size: 1.2vw;
-  color: #858585;
-  font-weight: 300;
-  border-bottom: 1px solid #858585;
-  padding-bottom: 1vw;
-}
-
-.footer-part2 ul {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: 1vw;
-  margin-top: 1vw;
-}
-
-.footer-part2 ul a {
-  font-size: 1.2vw;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
-  z-index: 9;
-  transition: all 0.3s;
-}
-
-.footer-part2 ul a::after {
-  mix-blend-mode: difference;
-  content: "";
-  height: 2.2vw;
-  width: 0%;
-  background-color: #ffffff;
-  display: block;
-  transition: all 0.3s;
-  position: absolute;
-  top: -25%;
-}
-
-.footer-part2 ul a:hover::after {
-  height: 2.2vw;
-  width: 100%;
-}
+const navOpenButton = document.querySelector("nav .top button i");
+const nav = document.querySelector("nav");
+const OpenerDiv = document.querySelector(".nav-main");
+let flag = 0;
+
+const navOpen = () => {
+  navOpenButton.addEventListener("click", () => {
+    if (flag === 0) {
+      gsap.to(nav, {
+        width: "80vw",
+        height: "auto",
+        duration: 0.75,
+        ease: "back.out(1)",
+      });
+
+      gsap.to(navOpenButton, {
+        attr: { class: "ri-close-line" },
+      });
+
+      flag = 1;
+    } else if (flag === 1) {
+      gsap.to(navOpenButton, {
+        attr: { class: "ri-menu-4-line" },
+      });
+
+      gsap.to(nav, {
+        width: "30vw",
+        height: "3vw",
+        duration: 0.7,
+        ease: "back.out(1)",
+      });
+
+      flag = 0;
+    } else {
+      console.error("flag inavailabe");
+    }
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      flag === 1 &&
+      !nav.contains(event.target) &&
+      !navOpenButton.contains(event.target)
+    ) {
+      gsap.to(navOpenButton, {
+        attr: { class: "ri-menu-4-line" },
+      });
+
+      gsap.to(nav, {
+        width: "30vw",
+        height: "3vw",
+        duration: 0.7,
+        ease: "back.out(1)",
+      });
+
+      flag = 0;
+    }
+  });
+};
+navOpen();
+
+const navDropAnimation = () => {
+  const nav = document.querySelector("nav");
+  document.addEventListener("DOMContentLoaded", () => {
+    gsap.from(nav, {
+      top: "-20%",
+      width: "0vw",
+      duration: 1.75,
+      ease: "back.out(1)",
+    });
+  });
+};
+
+navDropAnimation();
+
+const stringAnimation = () => {
+  var string = document.querySelector(".string");
+  var path = string.querySelector("path");
+
+  string.addEventListener("mousemove", (dets) => {
+    let rect = string.getBoundingClientRect();
+    let pathValue = `M 10 200 Q ${dets.x - rect.x} ${dets.y - rect.y} 1430 200`;
+
+    gsap.to(path, {
+      attr: { d: pathValue },
+      duration: 0.5,
+      ease: "power3.out",
+    });
+  });
+
+  string.addEventListener("mouseleave", () => {
+    gsap.to(path, {
+      attr: { d: "M 10 200 Q 500 200 1430 200" },
+      duration: 1,
+      ease: "elastic.out(1,0.3)",
+    });
+  });
+};
+stringAnimation();
+
+function sloganAnimation() {
+  const heroH1 = document.querySelector(".slogan h1");
+  heroH1.innerHTML = heroH1.textContent
+    .split("")
+    .map((letter) => `<span class="letter">${letter}</span>`)
+    .join("");
+  const letters = document.querySelectorAll(".letter");
+
+  gsap.from(letters, {
+    textShadow: "0 0 5px rgb(175, 175, 175)",
+    duration: 1,
+    stagger: 0.05,
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: ".slogan",
+      start: "top 80%",
+      end: "top 0%",
+      toggleActions: "restart pause reset",
+    },
+  });
+}
+sloganAnimation();
+
+letterAnimation = () => {
+  const heroH1 = document.querySelector(".hero h2");
+  heroH1.innerHTML = heroH1.textContent
+    .split("")
+    .map((letter) => `<span class="letter">${letter}</span>`)
+    .join("");
+  const letters = document.querySelectorAll(".letter");
+
+  const tl = gsap.timeline();
+
+  tl.to(letters, {
+    textShadow: "0 0 50px #00ff48",
+    color: "#ff",
+    duration: 0.5,
+    stagger: 0.05,
+    scrollTrigger: {
+      trigger: ".hero h2",
+      start: "top 100%",
+      end: "top 0%",
+    },
+  });
+
+  tl.to(letters, {
+    textShadow: "0 0 15px #00ff48",
+    delay: 0.5,
+    color: "#00ff48",
+    stagger: 0.05,
+    scrollTrigger: {
+      trigger: ".hero h2",
+      start: "top 80%",
+      end: "top 0%",
+    },
+  });
+
+  gsap.to(".hero h2 span", {
+    color: "#fff",
+    textShadow: "0 0 0px #fff",
+    delay: 4,
+  });
+};
+// letterAnimation();
+
+const cardAnimation = () => {
+  document
+    .querySelectorAll(".text-drop-line .drop-line")
+    .forEach((line, index) => {
+      gsap.from(line, {
+        opacity: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: line,
+          start: "top 85%",
+          end: "top 0%",
+          // toggleActions: "restart pause reset", //enable to rest animation on every scroll
+        },
+      });
+    });
+
+  const namer = document.querySelectorAll(".text-drop-line p");
+  namer.forEach((cont) => {
+    gsap.from(cont, {
+      opacity: 0,
+      transform: "translateX(-10%)",
+      duration: 2,
+      ease: "expo.out",
+      ease: "elastic.out(1,0.5)",
+      scrollTrigger: {
+        trigger: cont,
+        start: "top 85%",
+        end: "top 0%",
+        // toggleActions: "restart pause reset", //enable to rest animation on every scroll
+      },
+    });
+  });
+
+  const nameSpan = document.querySelectorAll(".text-drop-line p span");
+  nameSpan.forEach((cont) => {
+    gsap.from(cont, {
+      color: "#fff",
+      textShadow: "0 0 0px #fff",
+      delay: 0.35,
+      duration: 2,
+      ease: "circ.out",
+      scrollTrigger: {
+        trigger: cont,
+        start: "top 85%",
+        end: "top 0%",
+        // toggleActions: "restart pause reset", //enable to rest animation on every scroll
+      },
+    });
+  });
+};
+cardAnimation();
